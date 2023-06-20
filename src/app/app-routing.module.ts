30a4a3ac4a81d4ae7ed1371da92119d14eaf59fd
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-06-20 14:06:10 -0300

    NavBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { HeaderComponent } from './header/header.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ImagemComponent } from './imagem/imagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'Header', component: HeaderComponent},
+  {path: 'imagem', component: ImagemComponent},
+  {path: 'listar', component: ListarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
